8985c313b1595fceb2a315ef8fa3127a56c15d32
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2020-12-18 22:03:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Cattckbutton
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerMove = new System.Windows.Forms.Timer(this.components);
+            this.buttonGoal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timerMove
+            // 
+            this.timerMove.Tick += new System.EventHandler(this.timerMove_Tick);
+            // 
+            // buttonGoal
+            // 
+            this.buttonGoal.BackColor = System.Drawing.Color.Lime;
+            this.buttonGoal.Location = new System.Drawing.Point(425, 246);
+            this.buttonGoal.Name = "buttonGoal";
+            this.buttonGoal.Size = new System.Drawing.Size(50, 50);
+            this.buttonGoal.TabIndex = 0;
+            this.buttonGoal.UseVisualStyleBackColor = false;
+            this.buttonGoal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonGoal_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Счет:";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScore.Location = new System.Drawing.Point(87, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(29, 31);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "0";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonGoal);
+            this.MinimumSize = new System.Drawing.Size(1000, 700);
+            this.Name = "Form";
+            this.Text = "AIM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerMove;
+        private System.Windows.Forms.Button buttonGoal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
+
